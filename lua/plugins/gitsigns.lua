3c80fbd2d820a9e2c56760c0f1f032ebb7f0fcf8
gGameDevelop <ggdevelop356@gmail.com>
2024-11-08 17:41:23 +0000

    vim

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,5 +3,6 @@ return {
 	config = function ()
 		require("gitsigns").setup()
 		vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk_inline<CR>", { desc = "GitSigns preview hunk inline" })
+		vim.keymap.set("n", "<leader>gt", "<cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "GitSigns toggle current line blame" })
 	end
 }
